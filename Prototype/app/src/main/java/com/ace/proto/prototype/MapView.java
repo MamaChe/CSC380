68bed77a528b7b50de1ffdd39488d34cd21285e1
desiena <ace.desiena@gmail.com>
2015-11-04 11:48:02 -0500

    mess aroudn with scaling

--- Prototype/app/src/main/java/com/ace/proto/prototype/MapView.java
+++ Prototype/app/src/main/java/com/ace/proto/prototype/MapView.java
@@ -5,8 +5,13 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.DragEvent;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -19,34 +24,55 @@ import android.widget.ImageView;
  */
 public class MapView extends ImageView implements View.OnClickListener {
     private boolean rotated = false;
+    private static final Paint paint = new Paint();
+    private Path path = new Path();
     public AnimatorSet rotate;
     public static final float rotationAngle = 26, rotationYScale = (float).5;
+    public float userScale = 1;
     public MapView(Context context) {
         super(context);
-        init(context);
+        init();
     }
     public MapView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init();
     }
     public MapView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init();
     }
     public MapView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context);
+        init();
     }
     @Override
     public void onClick(View view){
         ((MapViewGroup)findViewById(R.id.MapViewGroup)).setActiveLayer(view);
     }
 
-    private void init(Context context){
+    private void init(){
+        paint.setStrokeWidth(7);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.STROKE);
         setPivotY(getHeight());
         rotate = new AnimatorSet();
         float rotationXScale = (float).95;
         rotate.play(ObjectAnimator.ofFloat(this, "rotationX", rotationAngle)).with(ObjectAnimator.ofFloat(this, "scaleY", rotationYScale)).with(ObjectAnimator.ofFloat(this, "scaleX", rotationXScale));
+    }
+    @Override
+    public void onDraw(Canvas canvas){
+        canvas.scale(userScale, userScale);
+        super.onDraw(canvas);
+        canvas.drawPath(path, paint);
+        //Log.d("drawing", "fs");
+    }
+    public void resetPath(){
+        path = new Path();
+        invalidate();
+    }
+    public void setPathStart(Location start) {
+        path.moveTo(start.getLongitude(), start.getLatitude());
     }
     public void rotate(){
         setPivotY(getHeight());
@@ -77,5 +103,8 @@ public class MapView extends ImageView implements View.OnClickListener {
         setPivotY(getHeight());
         setPivotX(getWidth() / 2);
     }
-
+    public void drawPath(Location sink){
+        path.lineTo(sink.getLongitude(), sink.getLatitude());
+        invalidate();
+    }
 }
--- Prototype/app/src/main/java/com/ace/proto/prototype/MapViewGroup.java
+++ Prototype/app/src/main/java/com/ace/proto/prototype/MapViewGroup.java
@@ -4,33 +4,34 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+
+import java.util.List;
 
 /**
  * Created by ace on 10/15/15.
  */
-public class MapViewGroup extends RelativeLayout {
+public class MapViewGroup extends RelativeLayout implements Gui, ScaleGestureDetector.OnScaleGestureListener {
     float downX, downY, trueDownY;
-    boolean rotated = false, zoomedOut = false, isClick = false;
+    boolean rotated = false, zoomedOut = true, isClick = false, isPan = false;
     int activeLayer;
-    private AnimatorSet rotate, unrotate;
+    ScaleGestureDetector scaleGestureDetector;
+    private AnimatorSet rotate, undoScroll;
 
     /*
     Constructors
      */
     public MapViewGroup(Context context){
         super(context);
+        scaleGestureDetector = new ScaleGestureDetector(context, this);
     }
     public MapViewGroup(Context context, AttributeSet attrs){
         super(context, attrs);
-        unrotate();
+        scaleGestureDetector = new ScaleGestureDetector(context, this);
     }
 
     //called by view shift button to toggle between single or multi-floor view
@@ -55,7 +56,7 @@ public class MapViewGroup extends RelativeLayout {
     }
     public void unrotate(){
         initializeAnimatorSets();
-        unrotate.start();
+        undoScroll.start();
         int childCount = getChildCount();
         Animator[] anims = new Animator[childCount];
         for(int i  = 0; i < childCount; i++) {
@@ -104,7 +105,7 @@ public class MapViewGroup extends RelativeLayout {
     //Define Animations
     public void initializeAnimatorSets() {
         rotate = new AnimatorSet();
-        unrotate = new AnimatorSet();
+        undoScroll = new AnimatorSet();
         Animator[] rotationAnimations = new Animator[getChildCount() * 2];
 
         //set rotate
@@ -118,8 +119,8 @@ public class MapViewGroup extends RelativeLayout {
             rotate.play(rotationAnimations[i]).with(rotationAnimations[i+1]);
         }
 
-        //set unrotate
-        unrotate.play(ObjectAnimator.ofInt(this, "scrollY", 0));
+        //set undoScroll
+        undoScroll.play(ObjectAnimator.ofInt(this, "scrollY", 0));
     }
 
 //    @Override
@@ -145,74 +146,133 @@ public class MapViewGroup extends RelativeLayout {
 //    }
     @Override
      public boolean onTouchEvent(MotionEvent event) {
-        float currentY = event.getY();
-        float currentX = event.getX();
-        int scrollByY, scrollByX;
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                downY = trueDownY = event.getY();
-                downX = event.getX();
-                isClick = true;
-                break;
+        scaleGestureDetector.onTouchEvent(event);
+        if(isPan){
+            isPan = false;
+        }else {
+            float currentY = event.getY();
+            float currentX = event.getX();
+            int scrollByY, scrollByX;
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    downY = trueDownY = event.getY();
+                    downX = event.getX();
+                    isClick = true;
+                    break;
 
-            case MotionEvent.ACTION_MOVE:
-                if(rotated) {
-                    float spaceForEach = (float)(Math.cos((double)MapView.rotationAngle) * MapView.rotationYScale * getHeight());
-                    scrollByY = (int) (downY - currentY);
-                    scrollByX = (int) (downX - currentX);
-                    if((scrollByY > 0 && scrollByY + getScrollY() < (getChildCount() - 1 - activeLayer) * spaceForEach)//scrolling up
-                            ||(scrollByY < 0 && scrollByY + getScrollY() > -1 * activeLayer * spaceForEach)) //scrolling down
-                    {
-                        scrollBy(0, scrollByY);
+                case MotionEvent.ACTION_MOVE:
+                    if (rotated) {
+                        float spaceForEach = (float) (Math.cos((double) MapView.rotationAngle) * MapView.rotationYScale * getHeight());
+                        scrollByY = (int) (downY - currentY);
+                        scrollByX = (int) (downX - currentX);
+                        if ((scrollByY > 0 && scrollByY + getScrollY() < (getChildCount() - 1 - activeLayer) * spaceForEach)//scrolling up
+                                || (scrollByY < 0 && scrollByY + getScrollY() > -1 * activeLayer * spaceForEach)) //scrolling down
+                        {
+                            scrollBy(0, scrollByY);
+                        }
+                        if (scrollByX > 1 || scrollByY > 1) {
+                            isClick = false;
+                        }
+
+                    } else if (!zoomedOut) {
+                        /*scrollByX = (int) (downX - currentX);
+                        scrollByY = (int) (downY - currentY);
+                        for (int i = 0; i < getChildCount(); i++) {
+                            getChildAt(i).scrollBy(scrollByX, scrollByY);
+                        }*/
+                    } else {
+                        //this part handles the draging of unrotated mapviews
+                        if (trueDownY - currentY <= 0 && activeLayer > 0) {
+                            View child = getChildAt(getChildCount() - activeLayer);
+                            child.setTranslationY(currentY - trueDownY - getHeight());
+                        } else if (trueDownY - currentY >= 0 && activeLayer < getChildCount() - 1) {
+                            View child = getChildAt(getChildCount() - activeLayer - 1);
+                            child.setTranslationY(currentY - trueDownY);
+                        } else {
+                            unrotate();
+                        }
                     }
-                    if(scrollByX > 1 || scrollByY > 1){
+                    downY = currentY;
+                    downX = event.getX();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (!rotated && zoomedOut) {
+                        double threshold = getHeight() * .2;
+                        if (trueDownY - currentY > threshold) {
+                            incrementActiveLayer();
+                        } else if (currentY - trueDownY > threshold) {
+                            decrementActiveLayer();
+                        } else {
+                            unrotate();
+                        }
+                    } else if (false) {
+                        rotate();
                         isClick = false;
                     }
-
-                }else if(zoomedOut){
-                    //scrollByX = (int) (downX - currentX);
-                    scrollByY = (int) (downY - currentY);
-                    for(int i = 0; i < getChildCount(); i ++){
-                        //getChildAt(i).scrollBy(scrollByX, scrollByY);
-                    }
-                }else{
-                    if(trueDownY - currentY <= 0 && activeLayer > 0){
-                        View child = getChildAt(getChildCount() - activeLayer);
-                        child.setTranslationY(currentY - trueDownY - getHeight());
-                    }else if(trueDownY - currentY >= 0 && activeLayer < getChildCount() - 1){
-                        View child = getChildAt(getChildCount() - activeLayer - 1);
-                        child.setTranslationY(currentY - trueDownY);
-                    }else{
-                        unrotate();
-                    }
-                }
-                downY = currentY;
-                downX = event.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                if(!rotated) {
-                    double threshold = getHeight() * .2;
-                    if (trueDownY - currentY > threshold) {
-                        incrementActiveLayer();
-                    } else if (currentY - trueDownY > threshold) {
-                        decrementActiveLayer();
-                    } else {
-                        unrotate();
-                    }
-                }else if (isClick){
-                    rotate();
-                    isClick = false;
-                }
+            }
         }
         return true;
+    }
+    public boolean onScaleBegin(ScaleGestureDetector detector){
+        return true;
+    }
+    public boolean onScale(ScaleGestureDetector detector){
+        if(!rotated) {
+            if (detector.getScaleFactor() > 1.2 || detector.getScaleFactor() < .8) {
+                isPan = true;
+            }
+            float scale = getChildAt(0).getScaleX() * detector.getScaleFactor();
+            if (scale > 1) {
+                zoomedOut = false;
+                setChildrenScale(scale);
+            } else if (scale == 1) {
+                zoomedOut = true;
+                setChildrenScale(scale);
+            } else {
+                zoomedOut = true;
+            }
+        }
+        return true;
+    }
+    public void onScaleEnd(ScaleGestureDetector detector){
+        unrotate();
+    }
+    private void setChildrenScale(float scale){
+        for(int i = 0;  i < getChildCount(); i++){
+            getChildAt(i).setScaleX(scale);
+            getChildAt(i).setScaleY(scale);
+            //setScaleX(scale);
+            //setScaleY(scale); 
+        }
     }
     public void setActiveLayer(View view){
 //        int layer = getChildCount() - indexOfChild(view) - 1;
 //        if(rotated && layer >= 0 && layer < getChildCount()){
 //            activeLayer = layer;
-//            unrotate();
+//            undoScroll();
 //            rotated = false;
 //        }
         rotate();
     }
+
+    @Override
+    public void drawPath(List<Location> locations) {
+        Location source = locations.get(0), sink;
+        for(int i = 0; i < getChildCount(); i++){
+            ((MapView)getChildAt(i)).resetPath();
+        }
+        ((MapView)getChildAt(source.getFloor())).setPathStart(source);
+        for(int i = 1; i < locations.size();i ++){
+            sink = locations.get(i);
+            if(source.getFloor() == sink.getFloor()){
+                ((MapView)getChildAt(source.getFloor())).drawPath(sink);
+            }else{
+
+                ((MapView)getChildAt(sink.getFloor())).setPathStart(sink);
+            }
+            source = sink;
+        }
+    }
+
+
 }
