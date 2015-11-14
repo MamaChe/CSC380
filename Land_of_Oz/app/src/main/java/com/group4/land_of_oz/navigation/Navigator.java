47c6c1f3569d30dbca01200a82eca55699368dae
Eric Guimarães <ericmguimaraes@gmail.com>
2015-11-14 09:58:00 -0500

    Added search by type on navigator

--- Land_of_Oz/app/src/main/java/com/group4/land_of_oz/navigation/Navigator.java
+++ Land_of_Oz/app/src/main/java/com/group4/land_of_oz/navigation/Navigator.java
@@ -25,26 +25,87 @@ public class Navigator {
         this.graph = graph;
     }
 
-    public List<Location> getBestPath(Location origin, Location destination, int type) {
+    public List<Location> getBestPath(Location origin, Location destination, int typeOfPreference) {
         GraphNode nodeOrigin = graph.getNodeByLocation(origin);
         GraphNode nodeDestination = graph.getNodeByLocation(destination);
-        return getBestPathAux(nodeOrigin, nodeDestination, type);
+        return getBestPathAux(nodeOrigin, nodeDestination, typeOfPreference);
     }
 
-    private List<Location> getBestPathAux(GraphNode origin, GraphNode destination, int type) {
+    private List<Location> getBestPathAux(GraphNode origin, GraphNode destination, int typeOfPreferences) {
         List<Location> path = null;
         if (origin.equals(destination)) {
             path = new ArrayList<Location>();
             path.add(origin.getLocation());
         } else {
+            //first the same floor
             for (GraphNode neighbor : origin.getNeighbors()) {
-                if ((type == Location.ELEVATOR && neighbor.getLocation().getType() != Location.STAIRWAY)
-                        || (type == Location.STAIRWAY && neighbor.getLocation().getType() != Location.ELEVATOR)
-                        || (type != Location.STAIRWAY && type != Location.ELEVATOR)) {
-                    path = getBestPathAux(neighbor, destination, type);
-                    if (path!=null) {
-                        path.add(neighbor.getLocation());
-                        break;
+                if(neighbor.getLocation().getFloor().getId()==origin.getLocation().getFloor().getId()) {
+                    if ((typeOfPreferences == Location.ELEVATOR && neighbor.getLocation().getType() != Location.STAIRWAY)
+                            || (typeOfPreferences == Location.STAIRWAY && neighbor.getLocation().getType() != Location.ELEVATOR)
+                            || (typeOfPreferences != Location.STAIRWAY && typeOfPreferences != Location.ELEVATOR)) {
+                        path = getBestPathAux(neighbor, destination, typeOfPreferences);
+                        if (path != null) {
+                            path.add(neighbor.getLocation());
+                            break;
+                        }
+                    }
+                }
+            }
+            //and then the rest
+            for (GraphNode neighbor : origin.getNeighbors()) {
+                if(neighbor.getLocation().getFloor().getId()!=origin.getLocation().getFloor().getId()) {
+                    if ((typeOfPreferences == Location.ELEVATOR && neighbor.getLocation().getType() != Location.STAIRWAY)
+                            || (typeOfPreferences == Location.STAIRWAY && neighbor.getLocation().getType() != Location.ELEVATOR)
+                            || (typeOfPreferences != Location.STAIRWAY && typeOfPreferences != Location.ELEVATOR)) {
+                        path = getBestPathAux(neighbor, destination, typeOfPreferences);
+                        if (path != null) {
+                            path.add(neighbor.getLocation());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if(path!=null) Collections.reverse(path);
+        return path;
+    }
+
+    public List<Location> getBestPath(Location origin, int specialType, int typeOfPreference) {
+        GraphNode nodeOrigin = graph.getNodeByLocation(origin);
+        return getBestPathAux(nodeOrigin, specialType, typeOfPreference);
+    }
+
+    private List<Location> getBestPathAux(GraphNode origin, int specialType, int typeOfPreferences) {
+        List<Location> path = null;
+        if (origin.getLocation().getType()==specialType) {
+            path = new ArrayList<Location>();
+            path.add(origin.getLocation());
+        } else {
+            //first the same floor
+            for (GraphNode neighbor : origin.getNeighbors()) {
+                if(neighbor.getLocation().getFloor().getId()==origin.getLocation().getFloor().getId()) {
+                    if ((typeOfPreferences == Location.ELEVATOR && neighbor.getLocation().getType() != Location.STAIRWAY)
+                            || (typeOfPreferences == Location.STAIRWAY && neighbor.getLocation().getType() != Location.ELEVATOR)
+                            || (typeOfPreferences != Location.STAIRWAY && typeOfPreferences != Location.ELEVATOR)) {
+                        path = getBestPathAux(neighbor, specialType, typeOfPreferences);
+                        if (path != null) {
+                            path.add(neighbor.getLocation());
+                            break;
+                        }
+                    }
+                }
+            }
+            //and then the rest
+            for (GraphNode neighbor : origin.getNeighbors()) {
+                if(neighbor.getLocation().getFloor().getId()!=origin.getLocation().getFloor().getId()) {
+                    if ((typeOfPreferences == Location.ELEVATOR && neighbor.getLocation().getType() != Location.STAIRWAY)
+                            || (typeOfPreferences == Location.STAIRWAY && neighbor.getLocation().getType() != Location.ELEVATOR)
+                            || (typeOfPreferences != Location.STAIRWAY && typeOfPreferences != Location.ELEVATOR)) {
+                        path = getBestPathAux(neighbor, specialType, typeOfPreferences);
+                        if (path != null) {
+                            path.add(neighbor.getLocation());
+                            break;
+                        }
                     }
                 }
             }
